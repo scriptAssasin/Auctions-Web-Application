5f83303df616f2f23df03a34122e9cedb02e8cf6
vagorellis <sdi1900203@uoa.gr>
2022-06-26 16:44:14 +0300

    docker working

--- ergasia-api/src/database/connection.py
+++ ergasia-api/src/database/connection.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 import databases
 from decouple import config
 from sqlalchemy import Column, ForeignKey, Integer, String, create_engine, MetaData, select, insert, update, or_
-
+import time
 
 host_server = config('HOST')
 db_server_port = config('PORT')
@@ -14,6 +14,33 @@ db_username = config('DB_USERNAME')
 db_password = config('DB_PASSWORD')
 ssl_mode = 'require'
 DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}'.format(db_username, db_password, host_server, db_server_port, database_name)
+
+def wait_for_db(db_uri):
+    """checks if database connection is established"""
+
+    _local_engine = create_engine(db_uri)
+
+    _LocalSessionLocal = sessionmaker(
+        autocommit=False, autoflush=False, bind=_local_engine
+    )
+
+    up = False
+    while not up:
+        try:
+            # Try to create session to check if DB is awake
+            db_session = _LocalSessionLocal()
+            # try some basic query
+            db_session.execute("SELECT 1")
+            db_session.commit()
+        except Exception as err:
+            print(f"Connection error: {err}")
+            up = False
+        else:
+            up = True
+        
+        time.sleep(2)
+
+wait_for_db(DATABASE_URL)
 
 # //////////// DB, ENGINE AND SESSION INIT  //////////////
 
