f94bc76224abe7e253e83544a5f822a3e639c57d
Aggelos Fakorellis <afakorellis@evolutionprojects.gr>
2022-08-29 17:05:58 +0300

    progr

--- ergasia-front/src/views/examples/modals/createAuction.js
+++ ergasia-front/src/views/examples/modals/createAuction.js
@@ -18,7 +18,10 @@ import {
 
 class CreateAuctionModal extends React.Component {
     state = {
-        defaultModal: false
+        defaultModal: false,
+        itemName: '',
+        itemCategories: '',
+        
     };
     toggleModal = state => {
         this.setState({
@@ -42,20 +45,53 @@ class CreateAuctionModal extends React.Component {
 
                         <Modal
                             className="modal-dialog-centered"
-                            size="sm"
+                            size="lg"
                             isOpen={this.state.formModal}
                             toggle={() => this.toggleModal("formModal")}
                         >
                             <div className="modal-body p-0">
                                 <Card className="bg-secondary shadow border-0">
                                     <CardHeader className="bg-transparent pb-5">
-                                        <h3 style={{textAlign: 'center'}}>Προσθήκη Δημοπρασίας</h3>
-                                        
+                                        <h3 style={{ textAlign: 'center' }}>Προσθήκη Δημοπρασίας</h3>
+
                                     </CardHeader>
                                     <CardBody className="px-lg-5 py-lg-5">
-                                       
+
                                         <Form role="form">
-                                            
+                                            <Row>
+                                                <Col sm='6'>
+                                                    <label>Όνομα προιόντος</label>
+                                                    <Input type='text' placeholder='Εισάγετε Όνομα Προιόντος' /> <br />
+                                                </Col>
+                                                <Col sm='6'>
+                                                    <label>Κατηγορία προιόντος (Πολλαπλές κατηγορίες διαχωρίζονται με , )</label>
+                                                    <Input type='text' placeholder='Εισάγετε Κατηγορίες Προιόντος' /> <br />
+
+                                                </Col>
+                                                <Col sm='6'>
+                                                    <label>Τιμή άμεσης αγοράς</label>
+                                                    <Input type='text' placeholder='Εισάγετε Τιμή Άμεσης Αγοράς' /> <br />
+
+                                                </Col>
+                                                <Col sm='6'>
+                                                    <label>Ελάχιστο μέγεθος πρώτης προσφοράς</label>
+                                                    <Input type='text' placeholder='Εισάγετε Ελάχιστο μέγεθος πρώτης προσφοράς' /> <br />
+
+                                                </Col>
+                                                <Col sm='6'>
+                                                    <label>Περιγραφή Προιόντος</label>
+                                                    <Input type='text' placeholder='Εισάγετε Περιγραφή Προιόντος' /> <br />
+
+                                                </Col>
+                                                <Col sm='12'>
+
+                                                    <Button color='success'>Υποβολή</Button>
+                                                </Col>
+                                            </Row>
+
+
+
+
                                         </Form>
                                     </CardBody>
                                 </Card>
