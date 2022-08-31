27d6f5c429ab3c8a2e0cfc517caf84e74a25c35c
vagorellis <sdi1900203@uoa.gr>
2022-09-01 00:31:29 +0300

    aggelos_final

--- ergasia-front/src/routes.js
+++ ergasia-front/src/routes.js
@@ -1,11 +1,8 @@
 
 import Index from "views/Index.js";
-import Profile from "views/examples/Profile.js";
 import Maps from "views/examples/Maps.js";
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
-import Tables from "views/examples/Tables.js";
-import Icons from "views/examples/Icons.js";
 import UserManagement from "views/examples/UserManagement.js";
 import UserDetails from "views/examples/UserDetails.js";
 import AuctionsManagement from "views/examples/AuctionsManagement.js";
